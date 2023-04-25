d120dfe016b0c32c2bab094e074fb1758e50ff2a
kennishida17 <ken.nishida@archibase.co.jp>
2023-04-25 20:54:04 +0900

    Create add and list tasks

--- hello/forms.py
+++ hello/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import MyModel
+from .models import Task
 
 class MyForm(forms.ModelForm):
     class Meta:
         model = MyModel
         fields = ('field1','field2')
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('title', 'description')
