8e376b0de67e5a225d7139879f0d45482e94ba88
kennishida17 <ken.nishida@archibase.co.jp>
2023-04-25 11:55:50 +0900

    Create html  helloworld

--- taskProject/settings.py
+++ taskProject/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
-,]
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
